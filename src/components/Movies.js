4acc0a7ea316b2483c311629034a2ce5ec3a1f2a
unknown <mohammadumar28@gmail.com>
2021-02-03 02:37:38 +0530

    Add styles to Movies component

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import styles from './Movies.module.scss';
 
 const Movies = ({ error, loading, movies }) => {
   if (error) {
@@ -11,11 +12,30 @@ const Movies = ({ error, loading, movies }) => {
   }
 
   if (movies) {
-    return movies.map(i => (
-      <div key={i.id}>
-        <Link to={`/movie/${i.id}`}>{i.title}</Link>
+    return (
+      <div className={styles.movie_grid}>
+        {movies.map(movie => (
+          <div
+            className={`${styles['movie-item']} ${styles['infos-container']}`}
+            key={movie.id}
+          >
+            <Link to={`/movie/${movie.id}`}>
+              <img
+                src={movie.poster_path
+                  ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                  : null}
+                alt="Movie Poster"
+              />
+              <div className={styles['infos-box']}>
+                <div className={styles['infos-one']}>{movie.release_date}</div>
+                <div className={styles['infos-two']}>{movie.original_title}</div>
+                <div className={styles['infos-three']}>{movie.vote_average}</div>
+              </div>
+            </Link>
+          </div>
+        ))}
       </div>
-    ));
+    );
   }
 
   return <div>Found none</div>;
